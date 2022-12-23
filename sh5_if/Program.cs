37b002898b1bb63d45f00daa60e27cb2d9b5b269
Dmitriy <dmitriyisaev2217736564@gmail.com>
2022-12-23 20:07:23 +0300

    Оператор if- ветвление

---
+++ sh5_if/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ВВЕДИТЕ ИМЯ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") //Чтобы имя Маша распознавалоось не зависимо от того как его ввел пользователь, надо вызвать операцию  ToLower()
+// Эта функция переведет все символы строки в нижний регистр
+{
+    Console.WriteLine("Ура!");
+
+}
+else
+{
+    Console.Write("Привет,    ");
+    Console.WriteLine(username);
+}
+
